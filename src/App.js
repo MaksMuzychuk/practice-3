af738faecb59a13ad3447c3e0515a2d01ab90ac7
Maks Muzychuk <maksmuzychuk@gmail.com>
2023-03-06 19:07:05 +0100

    add import queries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { useState } from 'react';
 import Characters from './components/Characters/Characters';
 import Sessions from './components/Sessions/Sessions';
 import { API } from 'aws-amplify';
+import {
+  getCharacter,
+  listCharacters,
+  getSession,
+  listSessions,
+} from './graphql/queries';
+import {
+  createCharacter as createCharacterMutation,
+  updateCharacter as updateCharacterMutation,
+  deleteCharacter as deleteCharacterMutation,
+  createSession as createSessionMutation,
+  updateSession as updateSessionMutation,
+  deleteSession as deleteSessionMutation,
+} from './graphql/mutations';
 
 function App() {
   const [selectedCharacter, setSelectedCharacter] = useState(null);
